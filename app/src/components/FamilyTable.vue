be0461f08710ea8cd5cb5342b07fe5fd9ab40dab
mattsii <msielecki2@gmail.com>
2022-07-25 21:24:18 -0700

    Create FamilyTable.vue

---
+++ app/src/components/FamilyTable.vue
@@ -0,0 +1,55 @@
+<template>
+<table id="firstTable">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Profession</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="row in rows">
+      <td>{{row.id}}</td>
+      <td>{{row.name}}</td>
+      <td>{{row.phone}}</td>
+      <td>{{row.profession}}</td>
+    </tr>
+  </tbody>
+</table>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    data () {
+        return {
+            loans: ''
+        }
+    },
+    created () {
+        axios.get('http://localhost:3100/person')
+        .then(
+            response => {
+                this.loans = response.body
+                // console.log(response)
+            })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
+}
+</script>
+
+<style type="text/css">
+	
+ul{
+  list-style: none;
+}
+li{
+  display: inline;
+}
+</style>
